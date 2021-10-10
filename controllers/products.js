0becb0585de46a230a9142c5a7d2874b99c5a65c
Mohammed Faisal <mfaisal718@gmail.com>
2021-10-10 17:26:50 -0400

    update & delete

--- controllers/products.js
+++ controllers/products.js
@@ -22,32 +22,32 @@ productsRouter.get("/", (req, res) => {
     });
 });
 // NEW
-//productsRouter.get("/new", (req, res) => {
-// res.send to check route
-// res.send("new");
-//res.render("new.ejs");
-//});
+productsRouter.get("/new", (req, res) => {
+    // res.send to check route
+    // res.send("new");
+    res.render("new.ejs");
+});
 // DELETE
-//productsRouter.delete("/:id", (req, res) => {
-//products.findByIdAndDelete(req.params.id, (err, data) => {
-//res.redirect('/products');
-//});
-//})
+productsRouter.delete("/:id", (req, res) => {
+    products.findByIdAndDelete(req.params.id, (err, data) => {
+        res.redirect('/products');
+    });
+})
 // UPDATE
-//productsRouter.put('/:id', (req, res) => {
-//if (req.body.completed === 'on') {
-//if checked, req.body.completed is set to 'on'
-//req.body.completed = true;
-//} else {
-//if not checked, req.body.completed is undefined
-//req.body.completed = false;
-//}
-//products.findByIdAndUpdate(req.params.id, req.//body, {
-//new: true
-//}, (error, updatedProducts) => {
-//res.redirect(`/products/${req.params.id}`);
-// });
-//});
+productsRouter.put('/:id', (req, res) => {
+    if (req.body.completed === 'on') {
+        //if checked, req.body.completed is set to 'on'
+        req.body.completed = true;
+    } else {
+        //if not checked, req.body.completed is undefined
+        req.body.completed = false;
+    }
+    products.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, (error, updatedProducts) => {
+        res.redirect(`/products/${req.params.id}`);
+    });
+});
 // CREATE
 productsRouter.post("/", (req, res) => {
     console.log('post create route hit')
@@ -59,13 +59,13 @@ productsRouter.post("/", (req, res) => {
 });
 
 // EDIT
-//productsRouter.get('/:id/edit', (req, res) => {
-//products.findById(req.params.id, (error, foundProducts) => {
-//res.render('edit.ejs', {
-//products: foundProducts
-//});
-// });
-//});
+productsRouter.get('/:id/edit', (req, res) => {
+    products.findById(req.params.id, (error, foundProducts) => {
+        res.render('edit.ejs', {
+            products: foundProducts
+        });
+    });
+});
 // SHOW
 productsRouter.get('/:id', (req, res) => {
     products.findById(req.params.id, (err, foundProducts) => {
